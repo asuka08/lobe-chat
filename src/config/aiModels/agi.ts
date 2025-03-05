307210037042b27ae3266a124aea40a13f4742a0
Wang Yuan <asuka08@sina.com>
2025-03-05 15:55:47 +0800

    增加几个模型配置

--- src/config/aiModels/agi.ts
+++ src/config/aiModels/agi.ts
@@ -17,7 +17,92 @@ const agiChatModels: AIChatModelCard[] = [
       output: 12,
     },
     type: 'chat',
-  }
+  },
+  {
+    abilities: {
+      functionCall: true,
+    },
+    contextWindowTokens: 128_000,
+    description:
+      'agi 4o mini',
+    displayName: 'agi-4o-mini',
+    enabled: true,
+    id: 'agi-4o-mini',
+    pricing: {
+      currency: 'CNY',
+      input: 12,
+      output: 12,
+    },
+    type: 'chat',
+  },
+  {
+    abilities: {
+      functionCall: true,
+    },
+    contextWindowTokens: 200_000,
+    description:
+      'agi sonnet 3.5',
+    displayName: 'agi-sonnet-3-5',
+    enabled: true,
+    id: 'agi-sonnet-3-5',
+    pricing: {
+      currency: 'CNY',
+      input: 12,
+      output: 12,
+    },
+    type: 'chat',
+  },
+  {
+    abilities: {
+      functionCall: true,
+    },
+    contextWindowTokens: 200_000,
+    description:
+      'agi sonnet 3.7',
+    displayName: 'agi-sonnet-3-7',
+    enabled: true,
+    id: 'agi-sonnet-3-7',
+    pricing: {
+      currency: 'CNY',
+      input: 12,
+      output: 12,
+    },
+    type: 'chat',
+  },
+  {
+    abilities: {
+      functionCall: true,
+    },
+    contextWindowTokens: 128_000,
+    description:
+      'agi deepseek v3',
+    displayName: 'agi-deepseek-v3',
+    enabled: true,
+    id: 'agi-deepseek-v3',
+    pricing: {
+      currency: 'CNY',
+      input: 12,
+      output: 12,
+    },
+    type: 'chat',
+  },
+  {
+    abilities: {
+      reasoning: true,
+    },
+    contextWindowTokens: 128_000,
+    description:
+      'agi deepseek r1',
+    displayName: 'agi-deepseek-r1',
+    enabled: true,
+    id: 'agi-deepseek-r1',
+    pricing: {
+      currency: 'CNY',
+      input: 12,
+      output: 12,
+    },
+    type: 'chat',
+  },
 ];
 
 export const allModels = [...agiChatModels];
--- src/config/modelProviders/agi.ts
+++ src/config/modelProviders/agi.ts
@@ -8,10 +8,54 @@ const Agi: ModelProviderCard = {
       contextWindowTokens: 128_000,
       description:
         'agi 4o',
-      displayName: 'Agi 4o',
+      displayName: 'Agi-4o',
       enabled: true,
       functionCall: true,
       id: 'agi-4o',
+    },
+    {
+      contextWindowTokens: 128_000,
+      description:
+        'agi 4o mini',
+      displayName: 'Agi-4o-mini',
+      enabled: true,
+      functionCall: true,
+      id: 'agi-4o-mini',
+    },
+    {
+      contextWindowTokens: 128_000,
+      description:
+        'agi sonnet 3.5',
+      displayName: 'agi-sonnet-3-5',
+      enabled: true,
+      functionCall: true,
+      id: 'agi-sonnet-3-5',
+    },
+    {
+      contextWindowTokens: 128_000,
+      description:
+        'agi sonnet 3.7',
+      displayName: 'agi-sonnet-3-7',
+      enabled: true,
+      functionCall: true,
+      id: 'agi-sonnet-3-7',
+    },
+    {
+      contextWindowTokens: 128_000,
+      description:
+        'agi deepseek v3',
+      displayName: 'agi-deepseek-v3',
+      enabled: true,
+      functionCall: true,
+      id: 'agi-deepseek-v3',
+    },
+    {
+      contextWindowTokens: 128_000,
+      description:
+        'agi deepseek r1',
+      displayName: 'agi-deepseek-r1',
+      enabled: true,
+      id: 'agi-deepseek-r1',
     },
   ],
   checkModel: 'agi-4o',
